9c2e2ab911a2e72e3340c2211ef43b85052d1958
JulianskyMe <juliakamynskaya@gmail.com>
2025-06-27 13:31:23 +0300

    commit

--- app/managers.py
+++ app/managers.py
@@ -4,3 +4,49 @@ from app.models import Actor
 
 
 # add manager here
+class ActorManager:
+    def __init__(self):
+        self._connection = sqlite3.connect("../actors_db.sqlite")
+        self.table_name = "actors"
+
+    def all(self):
+        actors_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        print(f"Actors_cursor: {actors_cursor}")
+        for row in actors_cursor:
+            print(row)
+
+    def create(self, first_name: str, last_name: str):
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def update(self, id_to_update: int, new_first_name: str, new_last_name: str):
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+        (new_first_name, new_last_name, id_to_update),
+        )
+        self._connection.commit()
+
+    def delete(self, id_to_delete: int):
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (id_to_delete,)
+        )
+        self._connection.commit()
+
+
+if __name__ == "__main__":
+    manager = ActorManager()
+    manager.create("Courtney", "Miller")
+    manager.all()
+    manager.update(3, "Courtney", "Topp")
+    manager.all()
+    manager.delete(3)
+    manager.all()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,13 @@
 from dataclasses import dataclass
+from django.db import models
 
 
 # add dataclass here
+@dataclass
+class Actor:
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    # def __init__(self, first_name, last_name) -> None:
+    #     self.first_name = first_name
+    #     self.last_name = last_name
